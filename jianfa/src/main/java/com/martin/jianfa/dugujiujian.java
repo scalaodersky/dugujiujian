41135498eb992d0fa4e60522808497695e15941c
martinscala <martinscala@126.com>
2018-12-06 13:10:02 +0800

    ybq

--- jianfa/src/main/java/com/martin/jianfa/dugujiujian.java
+++ jianfa/src/main/java/com/martin/jianfa/dugujiujian.java
@@ -2,6 +2,7 @@ package com.martin.jianfa;
 
 public class dugujiujian {
     public static void main(String[] args) {
+        System.out.println("1111111111111111111111111111111");
         System.out.println("hello,dugujiujian");
         System.out.println("hello,dugujiujian");
         System.out.println("hello,dugujiujian");
