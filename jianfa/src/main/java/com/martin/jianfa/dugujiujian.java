6bf9eb53ceadb264d65236abcf4aad230ecea87d
martinscala <martinscala@126.com>
2018-12-06 13:19:09 +0800

    ybq二体

--- jianfa/src/main/java/com/martin/jianfa/dugujiujian.java
+++ jianfa/src/main/java/com/martin/jianfa/dugujiujian.java
@@ -3,6 +3,7 @@ package com.martin.jianfa;
 public class dugujiujian {
     public static void main(String[] args) {
         System.out.println("1111111111111111111111111111111");
+        System.out.println("ybqybq");
         System.out.println("hello,dugujiujian");
         System.out.println("hello,dugujiujian");
         System.out.println("hello,dugujiujian");
