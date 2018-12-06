51faa85907d0a25ef37845a0c1ced162da93d568
scalaodersky <45628700+scalaodersky@users.noreply.github.com>
2018-12-06 12:02:58 +0800

    dugujiujian

---
+++ jianfa/src/main/java/com/martin/jianfa/dugujiujian.java
@@ -0,0 +1,11 @@
+package com.martin.jianfa;
+
+public class dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("hello,dugujiujian");
+        System.out.println("hello,dugujiujian");
+        System.out.println("hello,dugujiujian");
+        System.out.println("hello,dugujiujian");
+        System.out.println("hello,dugujiujian");
+    }
+}
